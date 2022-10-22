307f544757cb722a4ec0608de1c3ebfcb072a582
문경현 <2021125020@kau.kr>
2022-10-22 09:48:04 +0000

    1

--- baekjoon/1110.py
+++ baekjoon/1110.py
@@ -1,1 +1,18 @@
-hillhiilh
+N=int(input())
+start=N
+cicle=0
+while True:
+    N1 = int(N/10)
+    if N<10:
+        N2 = N
+    else:
+        N2 = N-(N1*10)
+    N=N1+N2
+    N=(N2*10) + (N-int(N/10)*10)
+    cicle=cicle+1
+    if start==N:
+        break 
+print(cicle)
+
+#2022.10.22 / 30 Minute / 파이썬 문법 되새기기 + 오랜만에 하는 코딩 몸풀기
+#1일차 시작
